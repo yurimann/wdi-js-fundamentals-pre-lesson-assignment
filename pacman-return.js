d8e8f5f47453bcc516ff66ced2e118a25fe96b95
Yuri <yuri.manahan@gmail.com>
2017-02-11 17:05:56 -0500

    Completed pacman

---
+++ pacman-return.js
@@ -0,0 +1,26 @@
+function addScore(currentScore, eaten) {
+  switch (eaten) {
+    case 'dot':
+      scoreToAdd = 10;
+      break;
+    case 'powerPellet':
+      scoreToAdd = 50;
+      break;
+    case 'firstGhost':
+      scoreToAdd = 200;
+      break;
+    case 'secondGhost':
+      scoreToAdd = 400;
+      break;
+  }
+  return currentScore + scoreToAdd;
+}
+
+var score = 0;
+
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'powerPellet');
+score = addScore(score, 'firstGhost');
+console.log('Your score: ' + score);
